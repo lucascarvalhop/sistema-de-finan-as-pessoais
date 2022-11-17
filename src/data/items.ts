713dc0ef01e4bc169d1924444bb097cfee010b19
Lucas Carvalho <lucas.pereiracarvalho@hotmail.com>
2022-11-17 08:49:34 -0300

    first-commit

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  {
+    date: new Date(2022, 9, 30),
+    category: 'food',
+    title: 'Mercado',
+    value: 387.82,
+  },
+  {
+    date: new Date(2022, 9, 30),
+    category: 'leisure',
+    title: 'Jogo de futebol',
+    value: 60,
+  },
+  {
+    date: new Date(2022, 9, 30),
+    category: 'rent',
+    title: 'Aluguel Apt',
+    value: 2300,
+  },
+  {
+    date: new Date(2022, 9, 18),
+    category: 'salary',
+    title: 'Salario',
+    value: 4500,
+  },
+];
